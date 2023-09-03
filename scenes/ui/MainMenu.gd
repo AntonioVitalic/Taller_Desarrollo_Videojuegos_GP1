b7f2935b707f4548d25b3f072a46be48d777f86f
jaker320 <oscar.salazar@ug.uchile.cl>
2023-09-03 17:47:27 -0300

    Minor changes
    edited gitignore
    Main menu refactor

--- scenes/ui/MainMenu.gd
+++ scenes/ui/MainMenu.gd
@@ -8,6 +8,17 @@ extends MarginContainer
 func _ready() -> void:
 	quit_button_mm.pressed.connect(_on_quit_pressed)
 	
+<<<<<<< Updated upstream
+=======
+func _on_start_pressed():
+	get_tree().change_scene_to_file("res://scenes/backgroundScenes/level0.tscn")
+	
+func _on_settings_pressed():
+	pass
+	
+func _on_credits_pressed():
+	pass
+>>>>>>> Stashed changes
 	
 func _on_quit_pressed():
 	get_tree().quit()
