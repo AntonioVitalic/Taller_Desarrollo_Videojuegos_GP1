8733bbfacc7c308134868bc8157e89a8b0851631
ANTONIO VITALIC <antonio.vitalic@C26.ALUMNOS>
2023-09-26 17:25:28 -0300

    Add deleted files

---
+++ scenes/ui/MainMenu.gd
@@ -0,0 +1,22 @@
+extends MarginContainer
+
+@onready var start_button_mm = %StartButtonMM
+@onready var settings_button_mm = %SettingsButtonMM
+@onready var credits_button_mm = %CreditsButtonMM
+@onready var quit_button_mm = %QuitButtonMM
+
+func _ready() -> void:
+	start_button_mm.pressed.connect(_on_start_pressed)
+	quit_button_mm.pressed.connect(_on_quit_pressed)
+	
+func _on_start_pressed():
+	get_tree().change_scene_to_file("res://scenes/backgroundScenes/level0.tscn")
+	
+func _on_settings_pressed():
+	pass
+	
+func _on_credits_pressed():
+	pass
+	
+func _on_quit_pressed():
+	get_tree().quit()
