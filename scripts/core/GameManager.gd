cbca4eb9bd7f709f60c6363d6db79dc445d32145
Ariel León <ariel.leon@ug.uchile.cl>
2023-10-01 03:41:37 -0300

    minor changes

--- scripts/core/GameManager.gd
+++ scripts/core/GameManager.gd
@@ -1,14 +1,13 @@
 extends Node
 
 var current_cp : Balloon
-var fred : Fred
-var coins
+var player : Fionna
+var coins : int
 signal gained_coins(int)
 
 
 
-
-func  gain_coins(coins_gained:int):
+func gain_coins(coins_gained:int):
 	coins += coins_gained
 	emit_signal("gained_coins", coins_gained)
 	print(coins)
--- scripts/interactables/coin.gd
+++ scripts/interactables/coin.gd
@@ -6,7 +6,6 @@ func _ready():
 	$AnimationPlayer.play("Idle")
 
 
-
 func _on_area_2d_area_entered(area):
 	GameManager.gain_coins(1)
 	queue_free()
