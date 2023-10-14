482519d8053634c01eff3fbfc5f605f18f56dde2
Gustavo Araya <galfaru99@gmail.com>
2023-10-14 18:55:05 -0300

    feat: Adds next level functionality
    Adds a new layer 4: Next_level in project.godot
    
    Adds next_level_area.tscn. An area2D scene where a collisionShape2D is implemented
    
    Adds next_level_area.gd which is the implementation of the next level functionality
    
    REMINDER: New levels have to have the new scene and add the path for the next level in the scene variables

---
+++ scenes/backgroundScenes/next_level_area.gd
@@ -0,0 +1,15 @@
+extends Area2D
+
+#@export var fred_spawn_location = Vector2.ZERO
+@export_file var next_scene_path = ""
+
+func _get_configuration_warnings():
+	if next_scene_path == "":
+		return "next_scene_path must be set for next_level to work"
+	else:
+		return ""
+
+
+func _on_body_entered(body):
+	if get_tree().change_scene_to_file(next_scene_path) != OK:
+		print("Error: Unavailable scene")
