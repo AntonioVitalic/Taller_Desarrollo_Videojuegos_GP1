755f8565dfdd270dd0f80102e81a904eb9640f29
Garayau <gustavoarayau@gmail.com>
2023-10-16 11:16:57 -0300

    fix: Fred's tongue moves in constant speed
    Fix to when fred's tongue moves too fast it's hitbox didn't collision with fionna's

--- tongue.gd
+++ tongue.gd
@@ -6,17 +6,19 @@ var direction := Vector2(0,0)	# The direction in which the tongue was shot
 
 
 
-const SPEED = 50	# The speed with which the tongue moves
+const SPEED = 2000	# The speed with which the tongue moves
 
 var flying = false	# Whether the tongue is moving through the air
 var hooked = false	# Whether the tongue has connected to a wall
 var fly
+var target = Vector2(0,0)
 
 # shoot() shoots the tongue in a given direction
 func shoot(dir: Vector2) -> void:
 	direction = dir
 	look_at(direction)
 	flying = true
+	target = get_global_mouse_position()
 
 # release() the tongue
 func release() -> void:
@@ -25,7 +27,7 @@ func release() -> void:
 
 func _physics_process(delta):
 	if flying:
-		tip.global_position = lerp(tip.global_position, direction, 10*delta )
+		tip.global_position = tip.global_position.move_toward(target, SPEED*delta)
 	elif hooked:
 		tip.global_position = fly.global_position
 	else: 
