6f68866ecd5d0203a08c032078349db061c3036f
AntonioVitalic <48534043+AntonioVitalic@users.noreply.github.com>
2023-11-20 12:27:34 -0300

    Fix: Changes timer in credits roll from 3 to 1 sec.

--- credits.gd
+++ credits.gd
@@ -6,8 +6,8 @@ extends CanvasLayer
 func _ready():
 	scroll_bar.step = 0.01
 	set_process(false)
-	await get_tree().create_timer(3).timeout
+	await get_tree().create_timer(1).timeout
 	set_process(true)
 
 func _process(delta):
-	scroll_bar.value += 1.1
+	scroll_bar.value += 1.0
